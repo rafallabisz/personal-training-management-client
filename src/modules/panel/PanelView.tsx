dcc9c08afadbaa77b2b631d394c60775d1c0af36
Rafal <rafal0710@gmail.com>
2019-11-19 12:30:18 +0100

    add redirect to loginPage when is refresh main page

--- src/modules/panel/PanelView.tsx
+++ src/modules/panel/PanelView.tsx
@@ -4,11 +4,14 @@ import UserPanelView from "../userPanel/trainersView/components/UserPanelView";
 import { useSelector } from "react-redux";
 import { Store } from "../auth/duck/auth.interfaces";
 import LoadingContainer from "../../utils/LoadingContainer";
+import { Redirect } from "react-router";
+import { routes } from "../../routes";
 
 interface PanelViewProps {}
 
 const PanelView: React.FC<PanelViewProps> = props => {
   const { currentUser, isFetching, error } = useSelector((state: Store) => state.user);
+  if (currentUser === undefined) return <Redirect to={routes.loginPage} />;
   const isTrainer = currentUser!.isTrainer;
   return (
     <>
