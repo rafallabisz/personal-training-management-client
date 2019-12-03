60c83acba3cd922f602e509f5874d5e82eb9fd98
Rafal Labisz <rafal0710@gmail.com>
2019-12-03 10:02:20 +0100

    add method to handle image

---
+++ src/utils/imageUtility.ts
@@ -0,0 +1,13 @@
+const toBase64 = (file: File) =>
+  new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = error => reject(error);
+  });
+
+const imageUtility = {
+  toBase64
+};
+
+export default imageUtility;
